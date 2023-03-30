2ced2a1637ebbf9379d92c287a3369574576f649
lariroque <cristina.lari_roque@hotmail.com>
2023-03-30 10:37:52 -0300

    implementacao do restante da calc e dos testes

--- calculadora.js
+++ calculadora.js
@@ -4,6 +4,19 @@ const calculadora = {
     }
 };
 
+subtracao(numero_1, numero_2) {
+    return numero_1 - numero_2;
+},
+
+multiplicacao(numero_1, numero_2) {
+    return numero_1 * numero_2;
+},
+
+divisao(numero_1, numero_2) {
+    return numero_1 / numero_2;
+},
+
+
 module.exports = {
     calculadora
 };
--- calculadora.test.js
+++ calculadora.test.js
@@ -5,5 +5,13 @@ test('soma de 2 numeros', () => {
 });
 
 test('subtracao de 2 numeros', () => {
+expect(calculadora.subtracao(3, 1)).toBe(2);
+});
 
-})
+test('multiplicacao de 2 numeros', () => {
+  expect(calculadora.multiplicacao(3, 2)).toBe(6);
+  });
+
+  test('divisão de 2 numeros', () => {
+    expect(calculadora.divisao(6, 2)).toBe(3);
+    });
